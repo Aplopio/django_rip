9cff452fac474217051e716826092f99473724b9
Ravi Shanker B <ravishanker414@gmail.com>
2015-01-09 18:53:18 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requirements = ['mock==1.0.1',
 
 setup(
     name='rip',
-    version='0.0.4',
+    version='0.0.4b',
     description='A python framework for writing restful APIs.',
     long_description=readme + '\n\n' + history,
     author='Aplopio developers',
