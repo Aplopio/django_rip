a4ca2322f7b2e64fa977c2afa837eda26e85702a
Vivek Chand <vivek@aplopio.com>
2015-02-24 12:23:11 +0530

    bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='rip',
-    version='0.0.5',
+    version='0.0.5a',
     description='A python framework for writing restful APIs.',
     long_description=readme + '\n\n' + history,
     author='Aplopio developers',
