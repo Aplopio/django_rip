d684f90a7062961391ab66693f86064d4667efe6
Karthik Damodara <karthikdamodara@users.noreply.github.com>
2015-05-06 11:00:38 +0530

    bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='rip',
-    version='0.0.8c',
+    version='0.0.9',
     description='A python framework for writing restful APIs.',
     long_description=readme + '\n\n' + history,
     author='Aplopio developers',
