6a812200e7ca34c2e91caa4cffe0f06c2a33243c
Raghu <raghu@aplopio.com>
2017-08-09 18:39:55 +0530

    modified default authentication to pass

--- rip/generic_steps/default_authentication.py
+++ rip/generic_steps/default_authentication.py
@@ -12,5 +12,4 @@ class DefaultAuthentication(object):
         self.schema_cls = schema_cls
 
     def authenticate(self, request):
-        return request if request.user \
-            else Response(is_success=False, reason=error_types.AuthenticationFailed)
+        return request
